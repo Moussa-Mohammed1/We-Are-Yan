695ac0501a7b891c3de3489bf2de292c65d55f4c
abdessamadelouarrag <abdessamad4804@gmail.com>
2026-03-26 15:22:59 +0100

    create migration of table users and annonces

---
+++ Back-End/database/migrations/2026_03_26_142141_create_table_annonces.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('annonces', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('beneficiary_id')->constrained('users')->onDelete('cascade');
+            $table->string('title');
+            $table->text('description');
+            $table->string('category');
+            $table->integer('quantity')->nullable();
+            $table->string('city');
+            $table->string('urgency')->default('medium');
+            $table->string('status')->default('pending');
+            $table->text('rejection_reason')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('annonces');
+    }
+};
