7c03164fefaa454d48c123b21548461d1497d4f7
abdessamadelouarrag <abdessamad4804@gmail.com>
2026-04-13 10:00:00 +0100

    Add donation relationships

--- Back-End/app/Models/Annonce.php
+++ Back-End/app/Models/Annonce.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Annonce extends Model
 {
@@ -39,5 +40,10 @@ class Annonce extends Model
     public function beneficiary(): BelongsTo
     {
         return $this->belongsTo(User::class, 'beneficiary_id');
+    }
+
+    public function donations(): HasMany
+    {
+        return $this->hasMany(Donation::class);
     }
 }
--- Back-End/app/Models/User.php
+++ Back-End/app/Models/User.php
@@ -5,6 +5,7 @@ namespace App\Models;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -61,5 +62,10 @@ class User extends Authenticatable
             'admin' => 'admin.dashboard',
             default => 'dashboard',
         };
+    }
+
+    public function donations(): HasMany
+    {
+        return $this->hasMany(Donation::class, 'donor_id');
     }
 }
