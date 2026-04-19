eaaf16a1cee710e0cc035f23dc667e333a1eec8d
abdessamadelouarrag <abdessamad4804@gmail.com>
2026-04-19 09:10:00 +0100

    Add realtime chat and Stripe support setup

--- Back-End/app/Events/Messages.php
+++ Back-End/app/Events/Messages.php
@@ -6,18 +6,32 @@ use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Broadcasting\PresenceChannel;
 use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class Messages implements ShouldBroadcast
+class Messages implements ShouldBroadcastNow
 {
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
     public function __construct(public $message) {}
 
     public function broadcastOn(): array
     {
         return [
             new PrivateChannel('chat.' . $this->message->conversation_id),
+        ];
+    }
+
+    public function broadcastWith(): array
+    {
+        return [
+            'message' => [
+                'id' => $this->message->id,
+                'content' => $this->message->content,
+                'sender_id' => $this->message->sender_id,
+                'sender_name' => $this->message->sender?->name ?? 'User',
+            ],
         ];
     }
 }
--- Back-End/routes/channels.php
+++ Back-End/routes/channels.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Models\Conversation;
 use Illuminate\Support\Facades\Broadcast;
 
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
+
+// Broadcast::channel('chat.{conversation}', function ($user, Conversation $conversation) {
+//     return (int) $user->id === (int) $conversation->donor_id
+//         || (int) $user->id === (int) $conversation->beneficiary_id;
+// });
